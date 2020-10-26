c347193891ee9df28da0a1cec2f6bed5a0b986f0
Hjalti Unnar Agustsson <hjalti@klappir.com>
2020-10-26 10:33:06 +0000

    Update doczrc.js

--- doczrc.js
+++ doczrc.js
@@ -2,7 +2,7 @@ export default {
   dest: "/docs",
   ignore: ["README.md"],
   description: "Documentation for Klappir platforms",
-  menu: [{ name: "Klappir Learn" }, {name: "Klappir Platform"}, {name: "EnviroMaster"}],
+  menu: [{ name: "Klappir Learn" }, {name: "EnviroMaster"}, {name: "Klappir Platform"}],
   themeConfig: {
     fonts: {
       body: 'Inter, Open Sans, Helvetica Neue, Helvetica, Arial, sans-serif',
